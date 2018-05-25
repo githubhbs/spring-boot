56c78564ec17dc42c6f60552af6d24a71055f9ba
githubhbs <39261005+githubhbs@users.noreply.github.com>
2018-05-25 16:46:58 +0800

    02

---
+++ src/main/javasource/bean/beanUser.java
@@ -0,0 +1,13 @@
+/***
+ *@author:huang_bing_shi
+ *@date: 2018 2018/5/23 18:04
+ *@description:购物项目
+ ***/
+package bean;
+
+public class beanUser {
+
+    public static void main(String[] args) {
+        System.out.println("sssssssss");
+    }
+}
